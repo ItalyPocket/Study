e2e3c13207579577e0fc056dc76bae037f272c0d
ItalyPocket <hyinkb1229@gmail.com>
2025-04-22 15:39:01 +0800

    add 2

--- Study/study.cs
+++ Study/study.cs
@@ -26,13 +26,81 @@
 //     string umi = miku;
 //     Console.WriteLine(umi);
 // }
-string stop = "go";
+
+   //循环加减乘除的计算器 纯粹If语句
+// string stop = "go";
+// for (Console.WriteLine("请输入所需要计算的内容"); ; Console.WriteLine("请输入所需要计算的内容"))
+// {
+//     string WhatYouEntered = Console.ReadLine();
+//     if (WhatYouEntered == "stop")
+//     {
+//         stop = "stop";
+//         Console.WriteLine("程序已停止");
+//         break;
+//     }
+//     while (WhatYouEntered == null || WhatYouEntered.Trim() == "")
+//     {
+//         Console.WriteLine("你输入的内容为空，请重新输入");
+//         WhatYouEntered = Console.ReadLine();
+//     }
+//     int Howlong = WhatYouEntered.Length;
+//     int miduem = WhatYouEntered.IndexOf("+");
+//     if (miduem == -1)
+//     {
+//         miduem = WhatYouEntered.IndexOf("-");
+//         if (miduem == -1)
+//         {
+//             miduem = WhatYouEntered.IndexOf("*");
+//             if (miduem == -1)
+//             {
+//                 miduem = WhatYouEntered.IndexOf("/");
+//                 if (miduem == -1)
+//                 {
+//                     Console.WriteLine("你输入的内容不符合计算要求，请重新输入");
+//                 }
+//                 else
+//                 {
+//                     float sum = nuber1(miduem) / nuber2(miduem);
+//                     Console.WriteLine("计算结果为" + sum);
+//                 }
+//             }
+//             else
+//             {
+//                 float sum = nuber1(miduem) * nuber2(miduem);
+//                 Console.WriteLine("计算结果为" + sum);
+//             }
+//         }
+//         else
+//         {
+//             float sum = nuber1(miduem) - nuber2(miduem);
+//             Console.WriteLine("计算结果为" + sum);
+//         }
+//     }
+//     else
+//     {
+//         float sum = nuber1(miduem) + nuber2(miduem);
+//         Console.WriteLine("计算结果为" + sum);
+//     }
+//     float nuber1(int miduem)
+//     {
+//         string nuber1 = WhatYouEntered.Substring(0, miduem);
+//         return float.Parse(nuber1);
+//     }
+//     float nuber2(int miduem)
+//     {
+//         string nuber2 = WhatYouEntered.Substring(miduem + 1, Howlong - miduem - 1);
+//         return float.Parse(nuber2);
+//     }
+// }
+
+
+
+// 循环加减乘除的计算器 用switch语句
 for (Console.WriteLine("请输入所需要计算的内容"); ; Console.WriteLine("请输入所需要计算的内容"))
 {
     string WhatYouEntered = Console.ReadLine();
     if (WhatYouEntered == "stop")
     {
-        stop = "stop";
         Console.WriteLine("程序已停止");
         break;
     }
@@ -41,6 +109,7 @@ for (Console.WriteLine("请输入所需要计算的内容"); ; Console.WriteLine
         Console.WriteLine("你输入的内容为空，请重新输入");
         WhatYouEntered = Console.ReadLine();
     }
+    int Fuhao = 0;
     int Howlong = WhatYouEntered.Length;
     int miduem = WhatYouEntered.IndexOf("+");
     if (miduem == -1)
@@ -53,31 +122,21 @@ for (Console.WriteLine("请输入所需要计算的内容"); ; Console.WriteLine
             {
                 miduem = WhatYouEntered.IndexOf("/");
                 if (miduem == -1)
-                {
-                    Console.WriteLine("你输入的内容不符合计算要求，请重新输入");
-                }
-                else
-                {
-                    float sum = nuber1(miduem) / nuber2(miduem);
-                    Console.WriteLine("计算结果为" + sum);
-                }
+                { Console.WriteLine("你输入的内容不符合计算要求，请重新输入"); }
+                else { Fuhao = 4; }
             }
-            else
-            {
-                float sum = nuber1(miduem) * nuber2(miduem);
-                Console.WriteLine("计算结果为" + sum);
-            }
+            else { Fuhao = 3; }
         }
-        else
-        {
-            float sum = nuber1(miduem) - nuber2(miduem);
-            Console.WriteLine("计算结果为" + sum);
-        }
+        else { Fuhao = 2; }
     }
-    else
+    else { Fuhao = 1; }
+    float sum = 0;
+    switch (Fuhao)
     {
-        float sum = nuber1(miduem) + nuber2(miduem);
-        Console.WriteLine("计算结果为" + sum);
+        case 1: sum = nuber1(miduem) + nuber2(miduem); break;
+        case 2: sum = nuber1(miduem) - nuber2(miduem); break;
+        case 3: sum = nuber1(miduem) * nuber2(miduem); break;
+        case 4: sum = nuber1(miduem) / nuber2(miduem); break;
     }
     float nuber1(int miduem)
     {
@@ -89,6 +148,5 @@ for (Console.WriteLine("请输入所需要计算的内容"); ; Console.WriteLine
         string nuber2 = WhatYouEntered.Substring(miduem + 1, Howlong - miduem - 1);
         return float.Parse(nuber2);
     }
+    Console.WriteLine("计算结果为" + sum);
 }
-
-
